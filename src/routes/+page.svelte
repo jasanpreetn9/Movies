de65058798276630196f99407aaa872420450fbf
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2022-10-23 16:47:51 -0500

    Revert "removed console log"
    This reverts commit fef8d4ccaaf9172586ba20e80a69d264da5bb9eb.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { each } from "svelte/internal";
 	import Popular from "../components/Popular.svelte";
 	import SearchMovie from "../components/SearchMovie.svelte";
+    export let data;
     const {popularMovies} = data
 
 </script>
