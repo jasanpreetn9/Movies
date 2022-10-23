fef8d4ccaaf9172586ba20e80a69d264da5bb9eb
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2022-10-23 16:38:30 -0500

    removed console log

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import { each } from "svelte/internal";
 	import Popular from "../components/Popular.svelte";
 	import SearchMovie from "../components/SearchMovie.svelte";
-    export let data;
     const {popularMovies} = data
 
 </script>
