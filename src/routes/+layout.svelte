8c236d6e9c4fde60dc49fa9040ea66647137fefb
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2022-10-23 16:55:57 -0500

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script>
-	import './global.css'
+	import './global.css';
 </script>
+
 <scelte:head>
 	<title>Movie</title>
 </scelte:head>
 <nav>
 	<a href="/">Movie DataBase</a>
 </nav>
+
+<slot />
+
+<footer>
+	<p>Data imported from <a href="../../static/tmdb.svg">TMDB</a></p>
+</footer>
 
 <style>
 	nav {
@@ -22,6 +29,9 @@
 		color: white;
 		text-decoration: none;
 	}
+	footer {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 </style>
-
-<slot />
