66b1ce6afeb7610f369359237d966ffaa171918c
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2022-10-28 12:59:15 -0500

    change card width and text size

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -7,7 +7,7 @@
 		<img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt={movie.title} />
 	</a>
 	<div class="description">
-		<h2>{movie.title}</h2>
+		<p>{movie.title}</p>
 	</div>
 </div>
 
@@ -18,7 +18,7 @@
 		border-radius: 1rem;
 		margin-bottom: 1rem;
 	}
-	h2 {
+	p {
 		font-size: 0.9em;
 	}
 	.description {
--- src/components/Popular.svelte
+++ src/components/Popular.svelte
@@ -14,7 +14,7 @@
 <style>
 	main {
 		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+		grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 		grid-column-gap: 1rem;
 		grid-row-gap: 2rem;
 	}
